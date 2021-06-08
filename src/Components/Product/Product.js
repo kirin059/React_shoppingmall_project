baa40206bf08b24c3b91267eca4e3cbd7a468346
saebom <boma91@gmail.com>
2021-06-08 20:51:35 +0900

    Add: file upload

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router';
+import axios from 'axios';
+import './Product.scss';
+
+const Product = (props) => {
+    let history = useHistory();
+
+    return (
+        <div className="row">
+            {
+                props.state.map((a, i) => {
+                    return (
+                        <div className="col-md-4" key={i} onClick={()=>{ history.push('/detail/' + props.state[i].id) }}>
+                            <img src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"} width="100%" />
+                            <h4> { props.state[i].title } </h4>
+                            <p> {props.state[i].content} & {props.state[i].price} </p>
+                            <p> 재고: { props.lestState[i] }</p>
+                        </div>
+                    )
+                })
+            }
+            <button className="btn btn-primary" onClick={() => {
+                axios.get('https://codingapple1.github.io/shop/data2.json')
+                .then((result) => {
+                    props.dispatch({ type: 'addList', payload: result.data })
+                    console.log(result.data)
+                    //setShoes([...shoes, ...result.data])
+                })
+                .catch(() => {
+                    console.log('failure')
+                  })
+            }} >더보기</button>
+           
+        </div>
+    );
+};
+
+function shoeProps(state) {
+    return {
+        state: state.reducer,
+        lestState: state.reducer2,
+        cartState: state.reducer3
+    }
+}
+export default connect(shoeProps)(Product);
+
